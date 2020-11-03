eeaa3da3a8c953c6f8104af627e8efb6f2a74624
Joseph Abraham <ekurubal24@gmail.com>
2020-11-03 22:01:15 +0300

    created new folder and files

---
+++ Redux/src/pages/SavedJobs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const SavedJobs = (props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>SAVED JOBS</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export {SavedJobs};
